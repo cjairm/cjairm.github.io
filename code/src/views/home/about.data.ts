172e6e8fe9ba6e086b39116d224f93896ea4a444
Carlos Mendez <carlos@hadaelectronics.com>
2022-02-21 19:18:51 -0600

    Version 2 of my portfolio - Code - typos

--- code/src/views/home/about.data.ts
+++ code/src/views/home/about.data.ts
@@ -21,20 +21,18 @@ const data = {
     where they can sell products.
     `,
     `
-    I speak two languages (English and Spanish). I'd love learn
+    I speak two languages (English and Spanish). I'd love to learn
     more languages. I enjoy traveling. I love to eat
-    every single regional food everywhere I go, and I give a
-    smile to everyone.
+    every single regional food everywhere I go, and I smile to everyone.
     `,
     `
-    Did you ask if I have a superpower? Yes,
-    I'm Mexican. To those that told me because I'm Mexican I
+    To those that told me because I'm Mexican I
     cannot shine, that the success was not available in my
     region (they may be partially right), well... I had the
-    opotunnity to born in a land full of potholes, challenges
-    and reasons to give up. A country that every day teach us
+    opportunity to be born in a land full of potholes, challenges
+    and reasons to give up. A country that every day teaches us
     how to handle disappointment and frustration. A country that
-    make us live in a constant necesity to reinvent ourselves
+    makes us live in a constant necessity to reinvent ourselves
     (not for pleasure but for survival).
     `,
     `
@@ -42,19 +40,19 @@ const data = {
     WE DON'T NEED THE EASY WAY. It's true that
     we don't have trustworthy institutions or accessible
     eduation, not even tranquility in the streets (in Mexico).
-    But what I DO have are balls, and desire, and ambition, and
+    But what I DO have balls, and desire, and ambition, and
     creativity, and pride, and an enviable resilience to defeat
-    any chanllenges on my way.
+    any challenges in my way.
     `,
     `
     Now, you need a meteorite to stop me. I'm hoping to do the
     things right to become an inspiration for the 120 million
     (population in Mexico) of folks that still don't believe
-    that it can be done. I want to became in another reason to
+    that it can be done. I want to became another reason to
     never give up, another role model that inspires everyone to
     continue bringing down walls.
     `,
-    `Let's make it happen!`,
+    "Let's make it happen!",
   ],
 } as const;
 
